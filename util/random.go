a66ad54440a3597395531634768e2a4e750fff47
achievagemilang <achievafuturagemilang@gmail.com>
2024-12-02 13:44:42 +0700

    chores: setup util functions

---
+++ util/random.go
@@ -0,0 +1,43 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomNumeric(min, max float64) float64 {
+	return min + float64(rand.Int63n(int64(max-min+1)))
+}
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandomString(n int) string{
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomBalance() float64 {
+	return RandomNumeric(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
